b5061822ac5d6305e8e98ee0039604446a8153d9
mdynur <91785121+mateuszd47@users.noreply.github.com>
2022-05-16 16:23:48 +0200

    reafactor 16bit input

---
+++ emulatorINTEL8086/src/Components/Inputs16bit.js
@@ -0,0 +1,203 @@
+import React, { useState } from "react";
+
+const Inputs16bit = () => {
+	const [radioOn, setRadioOn] = useState("");
+	const [radioWith, setRadioWith] = useState("");
+
+	const [values, setValues] = useState({
+		AX: "",
+		BX: "",
+		CX: "",
+		DX: "",
+	});
+
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		if (/^[0-9a-fA-F]+$/.test(value) || value === "") {
+			setValues({
+				...values,
+				[name]: value,
+			});
+		}
+	};
+
+  function mov(){
+    
+    // const { name, value } = e.target;
+    // setValues({
+    //   ...values,
+    //   [name]: value,
+    // });
+  }
+	return (
+		<section className='inputs16bit'>
+			<h3>16bit</h3>
+			<label>
+				AX:
+				<input
+					// className='radio'
+					type='text'
+					name='AX'
+					maxLength='4'
+					value={values.AX}
+					onChange={onChange}
+					placeholder='0000'
+				/>
+			</label>
+			<label>
+				BX:
+				<input
+					// className='radio'
+					type='text'
+					name='BX'
+					maxLength='4'
+					value={values.BX}
+					onChange={onChange}
+					placeholder='0000'
+				/>
+			</label>
+			<label>
+				CX:
+				<input
+					// className='radio'
+					type='text'
+					name='CX'
+					maxLength='4'
+					value={values.CX}
+					onChange={onChange}
+					placeholder='0000'
+				/>
+			</label>
+			<label>
+				DX:
+				<input
+					// className='radio'
+					type='text'
+					name='DX'
+					maxLength='4'
+					value={values.DX}
+					onChange={onChange}
+					placeholder='0000'
+				/>
+			</label>
+			<table className='tableZNA'>
+				<thead>
+					<tr>
+						<th>Z</th>
+						<th>NA</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<label>
+								AX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='with'
+									value='AX'
+									onChange={(e) => setRadioOn(e.target.value)}
+								/>
+							</label>
+						</td>
+						<td>
+							<label>
+								AX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='on'
+									value='AX'
+									onChange={(e) => setRadioWith(e.target.value)}
+								/>
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>
+								BX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='with'
+									value='BX'
+									onChange={(e) => setRadioOn(e.target.value)}
+								/>
+							</label>
+						</td>
+						<td>
+							<label>
+								BX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='on'
+									value='BX'
+									onChange={(e) => setRadioWith(e.target.value)}
+								/>
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>
+								CX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='with'
+									value='CX'
+									onChange={(e) => setRadioOn(e.target.value)}
+								/>
+							</label>
+						</td>
+						<td>
+							<label>
+								CX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='on'
+									value='CX'
+									onChange={(e) => setRadioWith(e.target.value)}
+								/>
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>
+								DX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='with'
+									value='DX'
+									onChange={(e) => setRadioOn(e.target.value)}
+								/>
+							</label>
+						</td>
+						<td>
+							<label>
+								DX:
+								<input
+									className='checkbox'
+									type='radio'
+									name='on'
+									value='DX'
+									onChange={(e) => setRadioWith(e.target.value)}
+								/>
+							</label>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			{console.log(radioOn)}
+			{console.log(radioWith)}
+		</section>
+	);
+};
+
+export default Inputs16bit;
