6321cf1ac06e86004e88aa1d7bcdd1d4afa3c0b6
mdynur <91785121+mateuszd47@users.noreply.github.com>
2022-05-21 13:07:27 +0200

    fix bug

--- emulatorINTEL8086/src/Components/Inputs16bit.js
+++ emulatorINTEL8086/src/Components/Inputs16bit.js
@@ -17,214 +17,221 @@ const Inputs16bit = () => {
 			setValues({
 				...values,
 				[name]: value,
-        [name]: localStorage.setItem([name], JSON.stringify(value))
+				[name]: localStorage.setItem([name], JSON.stringify(value)),
 			});
 		}
 	};
 
-  const reset = () => {
+	const reset = () => {
 		setValues({
 			AX: localStorage.setItem("AX", JSON.stringify("")),
-		  BX: localStorage.setItem("BX", JSON.stringify("")),
-		  CX: localStorage.setItem("CX", JSON.stringify("")),
-		  DX: localStorage.setItem("DX", JSON.stringify("")),
+			BX: localStorage.setItem("BX", JSON.stringify("")),
+			CX: localStorage.setItem("CX", JSON.stringify("")),
+			DX: localStorage.setItem("DX", JSON.stringify("")),
 		});
-    window.location.reload(false);
+		window.location.reload(false);
 	};
 
 	const mov = () => {
 		setValues({
 			[radioOn]: JSON.parse(localStorage.getItem(radioWith)),
 		});
-    localStorage.setItem([radioOn], JSON.stringify(JSON.parse(localStorage.getItem(radioWith))))
+		localStorage.setItem(
+			[radioOn],
+			JSON.stringify(JSON.parse(localStorage.getItem(radioWith)))
+		);
+		window.location.reload(false);
 	};
-  
+
 	const xchg = () => {
-    let tab = [JSON.parse(localStorage.getItem(radioOn)), JSON.parse(localStorage.getItem(radioWith))]
-    console.log(tab)
-    setValues({
-      [radioWith]: JSON.parse(localStorage.getItem(radioOn)),
+		let tab = [
+			JSON.parse(localStorage.getItem(radioOn)),
+			JSON.parse(localStorage.getItem(radioWith)),
+		];
+		console.log(tab);
+		setValues({
+			[radioWith]: JSON.parse(localStorage.getItem(radioOn)),
 			[radioOn]: JSON.parse(localStorage.getItem(radioWith)),
 		});
-    localStorage.setItem([radioWith], JSON.stringify(tab[0]))
-    localStorage.setItem([radioOn], JSON.stringify(tab[1]))
+		localStorage.setItem([radioWith], JSON.stringify(tab[0]));
+		localStorage.setItem([radioOn], JSON.stringify(tab[1]));
+		window.location.reload(false);
 	};
 	return (
 		<>
-      <div className='inputs16bit'>
-      <h3>16bit</h3>
-			<label>
-				AX:
-				<input
-					// className='radio'
-					type='text'
-					name='AX'
-					maxLength='4'
-					value={values.AX}
-					onChange={onChange}
-					placeholder='0000'
-				/>
-			</label>
-			<label>
-				BX:
-				<input
-					// className='radio'
-					type='text'
-					name='BX'
-					maxLength='4'
-					value={values.BX}
-					onChange={onChange}
-					placeholder='0000'
-				/>
-			</label>
-			<label>
-				CX:
-				<input
-					// className='radio'
-					type='text'
-					name='CX'
-					maxLength='4'
-					value={values.CX}
-					onChange={onChange}
-					placeholder='0000'
-				/>
-			</label>
-			<label>
-				DX:
-				<input
-					// className='radio'
-					type='text'
-					name='DX'
-					maxLength='4'
-					value={values.DX}
-					onChange={onChange}
-					placeholder='0000'
-				/>
-			</label>
-			<table className='tableZNA'>
-				<thead>
-					<tr>
-						<th>Z</th>
-						<th>NA</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							<label>
-								AX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='with'
-									value='AX'
-									onChange={(e) => setRadioWith(e.target.value)}
-								/>
-							</label>
-						</td>
-						<td>
-							<label>
-								AX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='on'
-									value='AX'
-									onChange={(e) => setRadioOn(e.target.value)}
-								/>
-							</label>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>
-								BX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='with'
-									value='BX'
-									onChange={(e) => setRadioWith(e.target.value)}
-								/>
-							</label>
-						</td>
-						<td>
-							<label>
-								BX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='on'
-									value='BX'
-									onChange={(e) => setRadioOn(e.target.value)}
-								/>
-							</label>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>
-								CX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='with'
-									value='CX'
-									onChange={(e) => setRadioWith(e.target.value)}
-								/>
-							</label>
-						</td>
-						<td>
-							<label>
-								CX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='on'
-									value='CX'
-									onChange={(e) => setRadioOn(e.target.value)}
-								/>
-							</label>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>
-								DX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='with'
-									value='DX'
-									onChange={(e) => setRadioWith(e.target.value)}
-								/>
-							</label>
-						</td>
-						<td>
-							<label>
-								DX:
-								<input
-									className='checkbox'
-									type='radio'
-									name='on'
-									value='DX'
-									onChange={(e) => setRadioOn(e.target.value)}
-								/>
-							</label>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-      </div>
-			
+			<div className='inputs16bit'>
+				<h3>16bit</h3>
+				<label>
+					AX:
+					<input
+						// className='radio'
+						type='text'
+						name='AX'
+						maxLength='4'
+						value={values.AX}
+						onChange={onChange}
+						placeholder='0000'
+					/>
+				</label>
+				<label>
+					BX:
+					<input
+						// className='radio'
+						type='text'
+						name='BX'
+						maxLength='4'
+						value={values.BX}
+						onChange={onChange}
+						placeholder='0000'
+					/>
+				</label>
+				<label>
+					CX:
+					<input
+						// className='radio'
+						type='text'
+						name='CX'
+						maxLength='4'
+						value={values.CX}
+						onChange={onChange}
+						placeholder='0000'
+					/>
+				</label>
+				<label>
+					DX:
+					<input
+						// className='radio'
+						type='text'
+						name='DX'
+						maxLength='4'
+						value={values.DX}
+						onChange={onChange}
+						placeholder='0000'
+					/>
+				</label>
+				<table className='tableZNA'>
+					<thead>
+						<tr>
+							<th>Z</th>
+							<th>NA</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<label>
+									AX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='with'
+										value='AX'
+										onChange={(e) => setRadioWith(e.target.value)}
+									/>
+								</label>
+							</td>
+							<td>
+								<label>
+									AX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='on'
+										value='AX'
+										onChange={(e) => setRadioOn(e.target.value)}
+									/>
+								</label>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>
+									BX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='with'
+										value='BX'
+										onChange={(e) => setRadioWith(e.target.value)}
+									/>
+								</label>
+							</td>
+							<td>
+								<label>
+									BX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='on'
+										value='BX'
+										onChange={(e) => setRadioOn(e.target.value)}
+									/>
+								</label>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>
+									CX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='with'
+										value='CX'
+										onChange={(e) => setRadioWith(e.target.value)}
+									/>
+								</label>
+							</td>
+							<td>
+								<label>
+									CX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='on'
+										value='CX'
+										onChange={(e) => setRadioOn(e.target.value)}
+									/>
+								</label>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>
+									DX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='with'
+										value='DX'
+										onChange={(e) => setRadioWith(e.target.value)}
+									/>
+								</label>
+							</td>
+							<td>
+								<label>
+									DX:
+									<input
+										className='checkbox'
+										type='radio'
+										name='on'
+										value='DX'
+										onChange={(e) => setRadioOn(e.target.value)}
+									/>
+								</label>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
 			<div className='buttons'>
 				<input className='button' type='button' value='MOV' onClick={mov} />
-				<input className='button' type='button' value='XCHG' onClick={xchg}/>
-				<input className='reset' type='button' value='RESET' onClick={reset}/>
+				<input className='button' type='button' value='XCHG' onClick={xchg} />
+				<input className='reset' type='button' value='RESET' onClick={reset} />
 			</div>
 		</>
 	);
 };
 
 export default Inputs16bit;
-
